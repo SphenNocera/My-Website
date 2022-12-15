41291df471925fa2267f7e3b718a8d1ad75521da
SphenNocera <noceraa432@gmail.com>
2022-12-15 00:22:38 -0500

    Removed lines

--- index.php
+++ index.php
@@ -1,23 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Scores Testing!</title>
+< !DOCTYPE html >
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Scores Testing!</title>
 
-    <script></script>
+          <script></script>
 </head>
-<body>
-    <h1>Hello World!</h1>
-    <h2>Welcome to my site! We will be up and running at some point!</h2>
+        <body>
+          <h1>Hello World!</h1>
+          <h2>Welcome to my site! We will be up and running at some point!</h2>
 
-    <p>Woah I'm doing it!!</p>
-
-    <div>
-        <p>Idk what this div will do, but testing auto deployment</p>
-    </div>
-
-    <?php
+          <?php
         include('test.php');
     ?>
 </body>
