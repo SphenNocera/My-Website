1c901df66dc41d8073a89c5938cab86a9f5b3bbf
SphenNocera <noceraa432@gmail.com>
2022-12-15 01:05:43 -0500

    New files for plants

---
+++ Runner.js
@@ -0,0 +1,40 @@
+let plant1, plant2, plant3;
+
+let water = [];
+
+function setup() {
+  createCanvas(600, 600);
+
+  plant1 = new Plant(100, height, 0.6, normalPot);
+  plant2 = new Plant(300, height, 1.45, tallPot);
+  plant3 = new Plant(500, height, 1, shortFat);
+}
+
+function draw() {
+  background(130, 199, 255);
+
+  plant1.draw();
+  plant2.draw();
+  plant3.draw();
+
+  plant1.grow();
+  plant2.grow();
+  plant3.grow();
+
+  for (let i in water) {
+    water[i].draw();
+    water[i].move();
+  }
+
+  if (mouseIsPressed) {
+    water.push(new Particle());
+  }
+
+  for (let i = water.length - 1; i >= 0; i--) {
+    if (water[i].pos.y > height) {
+      water.splice(i, 1);
+    }
+  }
+
+  //console.log(water.length);
+}
--- index.php
+++ index.php
@@ -5,7 +5,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
           <title>Scores Testing!</title>
 
-          <script></script>
+          <script src="libraries/p5.js"></script>
+    <script src="libraries/p5.collide2d.js"></script>
+    <script src="libraries/p5.dom.js"></script>
+    <script src="Runner.js"></script>
+    <script src="Classes.js"></script>
 </head>
         <body>
           <h1>Hello World!</h1>
