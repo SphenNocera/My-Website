59889af879364de574bd52da99925d5bd584fffd
SphenNocera <noceraa432@gmail.com>
2022-12-19 20:38:42 -0500

    l

--- test.php
+++ test.php
@@ -14,7 +14,7 @@
 
     //if(isset($_POST['fname'])){
         $fname = $_POST['fname'];
-        $sql = "INSERT INTO `MyGuests` (`id`, `firstname`, `lastname`, `email`, `reg_date`) VALUES (NULL, $fname, '', NULL, current_timestamp())";
+        $sql = "INSERT INTO `MyGuests` (`id`, `firstname`, `lastname`, `email`, `reg_date`) VALUES (NULL, $fname, NULL, current_timestamp())";
     //}
     //else {
     //echo "error...";
